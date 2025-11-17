40b8b413f38de151730634aa86808049a60b22a2
greetingsForAlek <alexpwoods@outlook.com>
2025-11-17 17:15:56 +0000

    Use environment variable for server port
    Updated the web server to use dynamic port from environment variables.

--- webserver.py
+++ webserver.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from threading import Thread
 
+port = int(os.environ.get("PORT", 8080)) # Just get the port to avoid 502 bad gateway error.
+
 app = Flask('')
 @app.route('/')
 def home():
     return "Discord bot OK"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
